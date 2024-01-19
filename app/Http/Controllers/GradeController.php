120cb1e65cfbdbfe25d6e5238fd9efda1144ae03
athdromeda <anixatho01@gmail.com>
2024-01-20 06:46:30 +0700

    code: Remove unused functions

--- app/Http/Controllers/GradeController.php
+++ app/Http/Controllers/GradeController.php
@@ -35,44 +35,4 @@ class GradeController extends Controller
         Grade::create($request->all());
         return response(['message' => 'New record created'], 200);
     }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
 }
